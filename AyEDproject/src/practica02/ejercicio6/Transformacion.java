b9a2d65cb203ce72d0c80aec49f416584d7d4860
Marimari2342 <marianroj@hotmail.com.ar>
2024-04-21 20:44:03 -0300

    commit punto transformacion

---
+++ AyEDproject/src/practica02/ejercicio6/Transformacion.java
@@ -0,0 +1,40 @@
+package practica02.ejercicio6;
+
+import practica02.ejercicio1y2.BinaryTree;
+
+public class Transformacion {
+
+    private BinaryTree<Integer> arbol;
+
+    public Transformacion(BinaryTree<Integer> unArbol) {
+        arbol = unArbol;
+    }
+
+    public BinaryTree<Integer> Suma() {
+        if(!this.arbol.isEmpty())
+            CalcularSuma(arbol);
+        return arbol;
+    }
+
+    private int CalcularSuma(BinaryTree<Integer>arbol){
+        int suma=0;
+        if (arbol.isLeaf()){        //Si es una hoja guardo un 0
+            suma=arbol.getData();
+            arbol.setData(0);
+            return suma;
+        }                           //Sino calculo suma de hijos derechos e izquierdos
+        if (arbol.hasLeftChild())
+            suma += CalcularSuma(arbol.getLeftChild());
+        if (arbol.hasRightChild())
+            suma += CalcularSuma(arbol.getRightChild());
+        int aux = arbol.getData();
+        arbol.setData(suma);
+        return aux+suma;
+    }
+
+    public BinaryTree<Integer> getArbolT() {
+        return arbol;
+    }
+
+
+}
