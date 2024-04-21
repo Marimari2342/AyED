3cad5fd43fcc2652db80de8f29c27a986baa29ce
Marimari2342 <marianroj@hotmail.com.ar>
2024-04-21 15:08:26 -0300

    commit punto5metodo

---
+++ AyEDproject/src/practica02/ejercicio5/ProfundidadDeArbolBinario.java
@@ -0,0 +1,32 @@
+package practica02.ejercicio5;
+
+import practica02.ejercicio1y2.BinaryTree;
+
+public class ProfundidadDeArbolBinario {
+
+    private BinaryTree<Integer> arbol;
+
+    public ProfundidadDeArbolBinario(BinaryTree<Integer> unArbol) {
+        arbol = unArbol;
+    }
+
+    public int sumaElementosProfundidad(int prof) {
+        if (!arbol.isEmpty())
+            return sumarElementos(arbol, prof, 0);
+        else
+            return 0;
+    }
+
+    private int sumarElementos(BinaryTree<Integer> arbol, int prof, int nivel) {
+        int suma = 0;
+        if (prof == nivel)
+            suma = arbol.getData();
+        else {
+            if (arbol.hasLeftChild())
+                suma = sumarElementos(arbol.getLeftChild(), prof, nivel++);
+            if (arbol.hasRightChild())
+                suma = sumarElementos(arbol.getRightChild(), prof, nivel++);
+        }
+        return suma;
+    }
+}
