9126fe02b8da9be24825a87d89780a5c3cfd68f8
Marimari2342 <marianroj@hotmail.com.ar>
2024-04-21 21:42:13 -0300

    commit borrador punto 7

--- AyEDproject/src/practica02/ejercicio7/ParcialArboles.java
+++ AyEDproject/src/practica02/ejercicio7/ParcialArboles.java
@@ -1,5 +1,18 @@
+//EN PROCESO
 package practica02.ejercicio7;
+
+import practica02.ejercicio1y2.BinaryTree;
 
 public class ParcialArboles {
 
+    private BinaryTree<Integer> arbol;
+
+    public ParcialArboles(BinaryTree<Integer> unArbol) {
+        arbol = unArbol;
+    }
+
+    public boolean isLeftTree (int num){
+        //primero busco el nodo que sea igual a num (metodo para buscar) 
+        return true; //luego evaluo lo que pide el ejercicio (metodo evaluar)
+    }
 }
