f3ea4ba5c5b6e2853fda2f6f4388baedf77eb3fb
Marimari2342 <marianroj@hotmail.com.ar>
2024-04-23 12:04:31 -0300

    commit punto 9

--- AyEDproject/src/practica02/ejercicio9/ParcialArboles.java
+++ AyEDproject/src/practica02/ejercicio9/ParcialArboles.java
@@ -6,12 +6,23 @@ public class ParcialArboles {
 
     public BinaryTree<SumaDiferencia> sumAndDif(BinaryTree<Integer> arbol) {
         BinaryTree<SumaDiferencia> arbolN = new BinaryTree<SumaDiferencia>();
-        evaluar(arbol, arbolN);
+        if (!arbol.isEmpty())  
+            evaluar(arbol, arbolN, 0, 0);
         return arbolN;
     }
 
-    private BinaryTree<SumaDiferencia> evaluar(BinaryTree<Integer> a, BinaryTree<SumaDiferencia> aN) {
-
+    private BinaryTree<SumaDiferencia> evaluar(BinaryTree<Integer> a, BinaryTree<SumaDiferencia> aN, int sum, int padre) {
+        int actual = a.getData();
+        SumaDiferencia aux = new SumaDiferencia(sum+a.getData(), a.getData()-padre);
+        aN.setData(aux);
+        if (a.hasLeftChild()){
+            aN.addLeftChild(new BinaryTree<SumaDiferencia>());
+            evaluar(a.getLeftChild(), aN.getLeftChild(), sum+actual,actual);
+        }
+        if (a.hasRightChild()) {
+            aN.addRightChild(new BinaryTree<SumaDiferencia>());
+            evaluar(a.getRightChild(), aN.getRightChild(), sum + actual, actual);
+        }
         return aN;
     }
 }
