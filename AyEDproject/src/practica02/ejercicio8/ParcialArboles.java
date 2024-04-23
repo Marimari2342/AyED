a0d1ae4c98c3229cd63236f601a89fa07cbf7201
Marimari2342 <marianroj@hotmail.com.ar>
2024-04-22 22:00:11 -0300

    commit punto8 recien planteando

---
+++ AyEDproject/src/practica02/ejercicio8/ParcialArboles.java
@@ -0,0 +1,18 @@
+package practica02.ejercicio8;
+
+import practica02.ejercicio1y2.BinaryTree;
+
+public class ParcialArboles {
+
+    private BinaryTree<Integer> arbol;
+
+    public ParcialArboles(BinaryTree<Integer> unArbol) {
+        arbol = unArbol;
+    }
+
+    //EN PROCESO
+    public boolean esPrefijo(BinaryTree<Integer> arbol1, BinaryTree<Integer> arbol2) {
+        return true;
+    }
+
+}
